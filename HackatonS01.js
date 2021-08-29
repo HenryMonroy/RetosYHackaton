f25d23090a102d1a1bced2588a6147d193cdbe8a
Henry2415 <henryrmg@hotmail.com>
2021-08-28 20:34:49 -0500

    Reto C01 - S01
    Solución del Reto de la primera clase de la primera semana

---
+++ HackatonS01.js
@@ -0,0 +1,21 @@
+//Ejercicio 01
+var admin;
+var name;
+
+name = "Jhon";
+admin = name;
+
+let alert = require('alert');
+alert(admin);
+
+//Ejercicio 02
+
+//Ejercicio 04
+
+//Ejercicio 05
+
+//Ejercicio 06
+
+//Ejercicio 07
+
+//Ejercicio 08
---
+++ RetoC01S01.js
@@ -0,0 +1,6 @@
+let peso = 75;
+let altura = 1.70;
+
+let imc = peso / (altura ** 2)
+
+console.log("Su IMC es " + imc + "(" + typeof imc + ")")
