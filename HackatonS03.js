6be1c61afbc0deb6d782239beb6ada88b37ce497
HenryMonroy <SV45340722@idat.edu.pe>
2021-09-11 10:32:03 -0500

    Partial HackatonS03

---
+++ HackatonS03.js
@@ -0,0 +1,64 @@
+/**
+ *      Ejercicio 00
+ */
+
+let unique = (arr) => {
+    arrTemp = arr.filter((value, index, self) => self.indexOf(value) === index);
+    console.log(arrTemp);
+}
+
+let values = ["Hare","Krishna","Hare","Krishna","Krishna","Krishna","Hare","Hare",":-O"];
+
+unique(values);
+console.log(values);
+
+
+/**
+ *      Ejercicio 01
+
+
+let ar = "er";
+ console.log(ar.split(""));
+ */
+
+/**
+ *     Ejecrcicio 02
+ */
+
+/**
+ *     Ejecrcicio 03
+ */
+
+let i = 3;
+
+while(i)
+{
+    alert(i--);//Se crea un bucle infinito porque la condición del while es siempre true
+}
+
+
+/**
+ *     Ejecrcicio 04
+ */
+
+let i = 0 ;
+while (++i < 5) console.log(i);//primero incremente luego evalua
+
+let j = 0;
+while (i++ < 5) console.log(j);//primero evalua luego incrementa
+
+/**
+ *     Ejecrcicio 05
+ */
+
+/**
+ *     Ejecrcicio 06
+ */
+
+/**
+ *     Ejecrcicio 07
+ */
+
+/**
+ *     Ejecrcicio 08
+ */
