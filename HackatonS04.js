2b5ecce95cd980336724605afe02c6b194e1f015
HenryMonroy <SV45340722@idat.edu.pe>
2021-09-18 10:32:56 -0500

    Update HackatonS04.js
    Ejercicio 02

--- HackatonS04.js
+++ HackatonS04.js
@@ -23,6 +23,10 @@ delay(3000).then(() => alert('runs after 3 seconds'));
  *      Ejercicio 02
  */
 
+promise.then(f1).catch(f2);//en este fragmento de código se ejecuta f1 y si devuelve null se ejecuta el catch.
+
+promise.then(f1,f2);//en este fragmento de código se ejecuta primero f1 devolviendo un resultado que captura f2
+
 /**
  *      Ejercicio 03
  */
